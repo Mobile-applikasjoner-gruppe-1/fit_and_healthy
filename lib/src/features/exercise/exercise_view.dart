a5f30a46bb6de81deef05effafbeb51a2c62420e
birgilv <birgittevik84@gmail.com>
2024-11-13 10:17:09 +0100

    Exception handling when workout list is empty

--- lib/src/features/exercise/exercise_view.dart
+++ lib/src/features/exercise/exercise_view.dart
@@ -1,4 +1,4 @@
-import 'package:fit_and_healthy/src/features/exercise/exercise_workout_card';
+import 'package:fit_and_healthy/src/features/exercise/exercise_workout_item.dart';
 import 'package:flutter/material.dart';
 import '../../../shared/models/exercise.dart';
 
@@ -12,16 +12,38 @@ class ExerciseView extends StatelessWidget {
 
  @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!'
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try adding a workout session!',
+          ),
+        ],
+      ),
+    );
+
+    if (workouts.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: workouts.length,
+        itemBuilder: (ctx, index) => ExerciseWorkoutItem(
+          workout: workouts[index]
+        ),
+      );
+
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Workouts'),
+        title: Text('Workouts!'),
       ),
-      body: ListView.builder(
-        itemCount: workouts.length,
-        itemBuilder: (context, index) {
-          return ExerciseWorkoutCard(workout: workouts[index]);
-        },
-      ),
+      body: content,
     );
   }
 }
+
