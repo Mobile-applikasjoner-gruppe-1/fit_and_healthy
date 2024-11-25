28e5b240e788e6b8f7aa2f090b05ddcc40cf2353
Anders Lund <61545005+AnderzL7@users.noreply.github.com>
2024-11-25 13:19:47 +0100

    Adds initial versions of workout and exercise repositories

--- lib/shared/models/exercise.dart
+++ lib/shared/models/exercise.dart
@@ -17,7 +17,7 @@ enum ExerciseSetType {
 
 // Keep the information about predefined exercises, like bench press or squat.
 class ExerciseInfoList {
-  final int id;
+  final String id;
   final String name;
   final ExerciseCategory exerciseCategory;
   final String info;
@@ -27,6 +27,37 @@ class ExerciseInfoList {
     required this.exerciseCategory,
     required this.info,
   });
+
+  factory ExerciseInfoList.fromFirebase(Map<String, dynamic> json) {
+    if (json['id'] is! String) {
+      throw TypeError();
+    }
+    if (json['name'] is! String) {
+      throw TypeError();
+    }
+    if (json['exerciseCategory'] is! ExerciseCategory) {
+      throw TypeError();
+    }
+    if (json['info'] is! String) {
+      throw TypeError();
+    }
+
+    return ExerciseInfoList(
+      id: json['id'],
+      name: json['name'],
+      exerciseCategory: json['exerciseCategory'],
+      info: json['info'],
+    );
+  }
+
+  Map<String, dynamic> toFirebase() {
+    return {
+      'id': id,
+      'name': name,
+      'exerciseCategory': exerciseCategory,
+      'info': info,
+    };
+  }
 }
 
 // Keep information about the sets repitition, weigth, and if it is a special set
@@ -43,7 +74,7 @@ class ExerciseSet {
 
 // The main Exercise
 class Exercise {
-  final int id;
+  final String id;
   final ExerciseInfoList exerciseInfoList;
   final List<ExerciseSet> sets;
   final String? note;
@@ -54,10 +85,40 @@ class Exercise {
     required this.sets,
     this.note,
   });
+
+  factory Exercise.fromFirebase(Map<String, dynamic> json) {
+    if (json['id'] is! int) {
+      throw TypeError();
+    }
+    if (json['exerciseInfoList'] is! ExerciseInfoList) {
+      throw TypeError();
+    }
+    if (json['sets'] is! List) {
+      throw TypeError();
+    }
+
+    return Exercise(
+      id: json['id'],
+      exerciseInfoList: json['exerciseInfoList'],
+      sets: json['sets'],
+      note: json['note'],
+    );
+  }
+
+  Map<String, dynamic> toFirebase() {
+    return {
+      'id': id,
+      'name': exerciseInfoList.name,
+      'exerciseCategory': exerciseInfoList.exerciseCategory,
+      'info': exerciseInfoList.info,
+      'note': note,
+    };
+    // TODO: Add sets as a subcollection
+  }
 }
 
 class ExerciseOther {
-  final int id;
+  final String id;
   final int length;
   final String time;
   final int intensity;
@@ -71,7 +132,7 @@ class ExerciseOther {
 
 // A single workout keeping track of a whole strength workout.
 class Workout {
-  final int id;
+  final String id;
   final String title;
   final String time;
   final DateTime dateTime;
@@ -83,4 +144,40 @@ class Workout {
     required this.dateTime,
     required this.exercises,
   });
+
+  factory Workout.fromFirebase(Map<String, dynamic> json) {
+    if (json['id'] is! String) {
+      throw TypeError();
+    }
+    if (json['title'] is! String) {
+      throw TypeError();
+    }
+    if (json['time'] is! String) {
+      throw TypeError();
+    }
+    if (json['dateTime'] is! DateTime) {
+      throw TypeError();
+    }
+    if (json['exercises'] is! List) {
+      throw TypeError();
+    }
+
+    return Workout(
+      id: json['id'],
+      title: json['title'],
+      time: json['time'],
+      dateTime: json['dateTime'],
+      exercises: json['exercises'],
+    );
+  }
+
+  Map<String, dynamic> toFirebase() {
+    return {
+      'id': id,
+      'title': title,
+      'time': time,
+      'dateTime': dateTime,
+      'exercises': exercises.map((e) => e.toFirebase()).toList(),
+    };
+  }
 }
--- lib/src/features/exercise/exercise_data.dart
+++ lib/src/features/exercise/exercise_data.dart
@@ -2,31 +2,31 @@ import 'package:fit_and_healthy/shared/models/exercise.dart';
 
 const List<ExerciseInfoList> sampleExerciseInfoList = [
   ExerciseInfoList(
-    id: 1,
+    id: '1',
     name: 'Bench Press',
     exerciseCategory: ExerciseCategory.chest,
     info: 'A classic chest exercise for building upper body strength.',
   ),
   ExerciseInfoList(
-    id: 2,
+    id: '2',
     name: 'Deadlift',
     exerciseCategory: ExerciseCategory.back,
     info: 'A compound exercise targeting back, legs, and core.',
   ),
   ExerciseInfoList(
-    id: 3,
+    id: '3',
     name: 'Squat',
     exerciseCategory: ExerciseCategory.legs,
     info: 'A key lower body exercise for building leg strength.',
   ),
   ExerciseInfoList(
-    id: 4,
+    id: '4',
     name: 'Shoulder Press',
     exerciseCategory: ExerciseCategory.shoulders,
     info: 'Targets shoulder muscles, enhancing upper body strength.',
   ),
   ExerciseInfoList(
-    id: 5,
+    id: '5',
     name: 'Bicep Curl',
     exerciseCategory: ExerciseCategory.biceps,
     info: 'An isolation exercise for strengthening the biceps.',
@@ -34,16 +34,21 @@ const List<ExerciseInfoList> sampleExerciseInfoList = [
 ];
 
 const List<ExerciseSet> sampleExerciseSets = [
-  ExerciseSet(repititions: 10, weight: 50.0, exerciseSetType: ExerciseSetType.warmup),
-  ExerciseSet(repititions: 8, weight: 75.0, exerciseSetType: ExerciseSetType.dropset),
-  ExerciseSet(repititions: 6, weight: 100.0, exerciseSetType: ExerciseSetType.tofailure),
+  ExerciseSet(
+      repititions: 10, weight: 50.0, exerciseSetType: ExerciseSetType.warmup),
+  ExerciseSet(
+      repititions: 8, weight: 75.0, exerciseSetType: ExerciseSetType.dropset),
+  ExerciseSet(
+      repititions: 6,
+      weight: 100.0,
+      exerciseSetType: ExerciseSetType.tofailure),
 ];
 
 const List<Exercise> sampleExercises = [
   Exercise(
-    id: 1,
+    id: '1',
     exerciseInfoList: ExerciseInfoList(
-      id: 1,
+      id: '1',
       name: 'Bench Press',
       exerciseCategory: ExerciseCategory.chest,
       info: 'A classic chest exercise for building upper body strength.',
@@ -51,14 +56,17 @@ const List<Exercise> sampleExercises = [
     sets: [
       ExerciseSet(repititions: 10, weight: 50.0),
       ExerciseSet(repititions: 8, weight: 55.0),
-      ExerciseSet(repititions: 6, weight: 60.0, exerciseSetType: ExerciseSetType.tofailure),
+      ExerciseSet(
+          repititions: 6,
+          weight: 60.0,
+          exerciseSetType: ExerciseSetType.tofailure),
     ],
     note: 'Focus on controlled movement to activate chest muscles.',
   ),
   Exercise(
-    id: 2,
+    id: '2',
     exerciseInfoList: ExerciseInfoList(
-      id: 2,
+      id: '2',
       name: 'Deadlift',
       exerciseCategory: ExerciseCategory.back,
       info: 'A compound exercise targeting back, legs, and core.',
@@ -66,14 +74,17 @@ const List<Exercise> sampleExercises = [
     sets: [
       ExerciseSet(repititions: 10, weight: 70.0),
       ExerciseSet(repititions: 8, weight: 80.0),
-      ExerciseSet(repititions: 5, weight: 90.0, exerciseSetType: ExerciseSetType.dropset),
+      ExerciseSet(
+          repititions: 5,
+          weight: 90.0,
+          exerciseSetType: ExerciseSetType.dropset),
     ],
     note: 'Keep back straight to avoid injury.',
   ),
   Exercise(
-    id: 3,
+    id: '3',
     exerciseInfoList: ExerciseInfoList(
-      id: 3,
+      id: '3',
       name: 'Squat',
       exerciseCategory: ExerciseCategory.legs,
       info: 'A key lower body exercise for building leg strength.',
@@ -81,7 +92,10 @@ const List<Exercise> sampleExercises = [
     sets: [
       ExerciseSet(repititions: 12, weight: 40.0),
       ExerciseSet(repititions: 10, weight: 50.0),
-      ExerciseSet(repititions: 8, weight: 60.0, exerciseSetType: ExerciseSetType.warmup),
+      ExerciseSet(
+          repititions: 8,
+          weight: 60.0,
+          exerciseSetType: ExerciseSetType.warmup),
     ],
     note: 'Engage core during the squat.',
   ),
@@ -89,17 +103,17 @@ const List<Exercise> sampleExercises = [
 
 final List<Workout> sampleWorkouts = [
   Workout(
-    id: 1,
+    id: '1',
     title: 'Chest & Back Day',
     time: '10:00 AM',
     dateTime: DateTime.now(),
     exercises: [
-      sampleExercises[0], 
+      sampleExercises[0],
       sampleExercises[1],
     ],
   ),
   Workout(
-    id: 2,
+    id: '2',
     title: 'Leg Day',
     time: '3:00 PM',
     dateTime: DateTime.now().add(Duration(days: 1)),
@@ -108,7 +122,7 @@ final List<Workout> sampleWorkouts = [
     ],
   ),
   Workout(
-    id: 3,
+    id: '3',
     title: 'Upper Body Strength',
     time: '5:00 PM',
     dateTime: DateTime.now().add(Duration(days: 2)),
