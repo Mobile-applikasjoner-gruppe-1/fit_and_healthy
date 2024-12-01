0e675d4c23ecf465c263a526497547407dc6a4d6
birgilv <birgittevik84@gmail.com>
2024-12-01 11:59:05 +0100

    Updated to use date- and time picker

--- lib/src/features/exercise/screens/exercise_add_workout.dart
+++ lib/src/features/exercise/screens/exercise_add_workout.dart
@@ -2,24 +2,27 @@ import 'package:fit_and_healthy/shared/models/exercise.dart';
 import 'package:fit_and_healthy/src/nested_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
 /**
- * The AddWorkoutView widget provides functionality for adding and managing
- * a new workout session.
- * 
- * Features:
- * - Displays the total weight in the workout.
- * - Includes buttons for adding exercises or getting exercise suggestions.
- * - Contains a floating action button for additional options.
+ * The `AddWorkout` widget, provides a user interface 
+ * for adding and managing a new workout session. It includes:
+ * - A form for entering workout details (title, date, time).
+ * - Buttons for navigating to add exercises and submitting the workout.
+ * - The ability to select a date and time using pickers.
  */
+
 class AddWorkout extends StatefulWidget {
+  /**
+   * Constructor for the `AddWorkout` widget.
+   * 
+   * @param workouts A list of `Workout` objects used to manage and display exercises in the workout session.
+   */
   const AddWorkout({super.key, required this.workouts});
 
   static const route = '/exercise';
 
-  final List<Workout> workouts;
+  final List<Workout> workouts; // The list of workouts
 
   @override
   State<AddWorkout> createState() {
@@ -28,129 +31,119 @@ class AddWorkout extends StatefulWidget {
 }
 
 class _AddWorkoutState extends State<AddWorkout> {
-  String _title = 'New Workout';
-  DateTime _selectedDate = DateTime.now(); 
-  TimeOfDay _selectedTime = TimeOfDay.now();
-  List<Exercise> _exercises = [];
-  
-  /**
-   * Opens the date picker dialog and updates the selected date.
-   */
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (pickedDate != null && pickedDate != _selectedDate) {
-      setState(() {
-        _selectedDate = pickedDate; // Update the selected date
-      });
-    }
-  }
+  String _title = 'New Workout'; // Default title of the workout
+  DateTime _selectedDate = DateTime.now(); // Selected date
+  TimeOfDay _selectedTime = TimeOfDay.now(); // Selected time
 
   /**
-   * Opens the time picker dialog and updates the selected time.
+   * Navigates to the Add Exercise screen using `GoRouter` to 
+   * navigate to the add-exercise route.
+   * 
+   * @param context The current BuildContext of the widget.
    */
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-    );
-    if (pickedTime != null && pickedTime != _selectedTime) {
-      setState(() {
-        _selectedTime = pickedTime; // Update the selected time
-      });
-    }
-  }
-
-  void navigateToAddExercise(BuildContext context) {
+  void _navigateToAddExercise(BuildContext context) {
     context.push('${AddWorkout.route}/add-exercise');
-  }
-
-  List<Exercise> getExercisesFromWorkout(Workout workout) {
-    return workout.exercises;
-  }
-
-  void _createWorkout() {
-    final newWorkout = Workout(
-      id: UniqueKey().toString(),
-      title: _title,
-      time: _formatTime(_selectedTime),
-      dateTime: _selectedDate,
-      exercises: _exercises,
-    );
-  }
-
-  /**
-   * The formatDate method, format the date as 'MMM d, yyyy'. 
-   * This could be for example, 'November 13, 2024'
-   */
-  String _formatDate(DateTime date) {
-    return DateFormat('MMMM d, yyyy').format(date);
-  }
-
-  String _formatTime(TimeOfDay time) {
-    final now = DateTime.now();
-    final timeAsDateTime = DateTime(now.year, now.month, now.day, time.hour, time.minute);
-    return DateFormat('hh:mm a').format(timeAsDateTime);
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget content = SingleChildScrollView(
-      child: Center(
-        child: Padding(
-        padding: const EdgeInsets.all(16.0), 
-        child: Form(
-          child: Column(
-            children: [
-              Column(
-                children:[
+    return NestedScaffold(
+      appBar: AppBar(
+        title: const Text('Add workout'),
+        centerTitle: true,
+        actions: [
+          /**
+           * Finalizes the workout creation process.
+           * 
+           * Displays a success message and navigates back to the previous screen.
+           */
+          TextButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Workout added successfully!'),
+                  duration: Duration(seconds: 2),
+                  backgroundColor: Colors.green,
+                ),
+              );
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'Finish',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Column(
+                children: [
                   TextFormField(
-                    decoration: InputDecoration(
-                      label: Text('Title')
-                    ),
-                    initialValue: 'New workout',
+                    decoration: const InputDecoration(label: Text('Title')),
+                    initialValue: _title,
+                    onChanged: (value) => setState(() {
+                      _title = value;
+                    }),
                   ),
-                  GestureDetector(
-                    onTap: () => _selectDate(context),
-                    child: AbsorbPointer(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          label: Text('Date'),
-                        ),
-                        controller: TextEditingController(
-                          text: _formatDate(_selectedDate),
-                        ),
-                      ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () async {
+                      final DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: _selectedDate,
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime.now(),
+                      );
+                      if (pickedDate != null) {
+                        setState(() {
+                          _selectedDate = pickedDate;
+                        });
+                      }
+                    },
+                    child: Row(
+                      children: [
+                        const Icon(Icons.calendar_today),
+                        const SizedBox(width: 8),
+                        Text('${DateFormat.EEEE().format(_selectedDate)} ${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}',)
+                      ]
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () => _selectTime(context),
-                    child: AbsorbPointer(
-                      child: TextFormField(
-                        decoration: const InputDecoration(label: Text('Time')),
-                        controller: TextEditingController(
-                          text: _formatTime(_selectedTime),
-                        ),
-                      ),
-                    ),
+                  TextButton(
+                    onPressed: () async {
+                      final TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: _selectedTime,
+                      );
+                      if (pickedTime != null) {
+                        setState(() {
+                          _selectedTime = pickedTime;
+                        });
+                      }
+                    },
+                    child: Row(
+                      children: [
+                        const Icon(Icons.access_alarms_outlined),
+                        const SizedBox(width: 8),
+                        Text(' ${_selectedTime.format(context)}',),
+                      ],
+                    )
                   ),
-                  
                   const SizedBox(height: 24),
                   ElevatedButton(
-                    onPressed: () => {
-                      navigateToAddExercise(context),
-                    },
+                    onPressed: () => _navigateToAddExercise(context),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
                     ),
-                    child: Text(
-                      'Add Exercise', 
+                    child: const Text(
+                      'Add Exercise',
                       style: TextStyle(
-                        fontSize: 16, 
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
@@ -170,44 +163,13 @@ class _AddWorkoutState extends State<AddWorkout> {
                     color: Colors.blue,
                   ),
                   const SizedBox(height: 16),
-
-                  // Add list of all exercises within this workout
+                  // Display the list of exercises here
                 ],
-              ),
-            ],
-          ))
-        ),
-      ),
-    );
-
-    return NestedScaffold(
-      appBar: AppBar(
-        title: Text('Add workout'),
-        centerTitle: true,
-        actions: [
-          TextButton(
-            onPressed: () {
-              //_createWorkout();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: const Text('Workout added successfully!'),
-                  duration: const Duration(seconds: 2),
-                  backgroundColor: Colors.green,
-                ),
-              );
-
-              Navigator.pop(context);
-            },
-            child: const Text(
-              'Finish',
-              style: TextStyle(
-                color: Colors.blue, 
               ),
             ),
           ),
-        ],
+        ),
       ),
-      body: content,
     );
   }
 }
